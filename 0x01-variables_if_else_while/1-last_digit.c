4bf1299c49f662fd1abe9c71fb758b2a354c8613
Steye3 <teyesolomon427@gmail.com>
2023-02-19 07:14:10 -0800

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,17 @@ if (a > 5)
 {
 printf("last digit of %d is %d and is greater than 5\n", n, a);
 }
-else if (a < 6)
+
+else if (a == 0)
 {
-printf("last digit of %d is %d and is less than 6 and not 0 \n", n, a);
+printf("last digit of %d is %d and is 0 \n", n, a);
 }
+
 else
+(a < 6)
 {
-printf("last digit of %d is %d and is 0\n", n, a);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
+
 }
 return (0);
 }
