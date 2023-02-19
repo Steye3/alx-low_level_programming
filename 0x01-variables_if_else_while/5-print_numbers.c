95ed1924cd9d9aef8d048265afe2fc38836ef148
Steye3 <teyesolomon427@gmail.com>
2023-02-19 08:37:14 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - printing numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+
+return (0);
+}
