27444f09e84e9516950867660fc37a19d2b2529e
Steye3 <teyesolomon427@gmail.com>
2023-02-19 10:10:06 -0800

    bsbsb

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 int num;
 char la;
 for (num = 0; num < 10; num++)
-putchar((num % 10) + 'o');
+putchar((num % 10) + '0');
 for (la = 'a'; la <= 'f'; la++)
 putchar(la);
 putchar('\n');
