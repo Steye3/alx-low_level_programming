8d16178bf44da779f04f1ecf37a322e8a7b78b01
Steye3 <teyesolomon427@gmail.com>
2023-02-19 07:49:23 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - print out lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+
+}
+putchar('\n');
+return (0);
+}
+
