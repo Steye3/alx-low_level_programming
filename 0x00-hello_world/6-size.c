223bc2e44823d806194d53ad782a466175672ed7
Steye3 <teyesolomon427@gmail.com>
2023-02-18 17:50:06 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ printf("Size of a int : %d byte(s)\n", sizeof(int));
 printf("Size of a float : %d byte(s)\n", sizeof(float));
 printf("Size of a char : %d byte(s)\n", sizeof(char));
 printf("Size of a long int : %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int : %d", sizeof(long long int));
+printf("Size of a long long int : %d byte(s)\n", sizeof(long long int));
 return (0);
 
 
