a674869eb321a8a4bdea8cf1fe04c0033f8e13d0
Steye3 <teyesolomon427@gmail.com>
2023-02-18 18:12:23 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
 /**
- 
-* main - a program that prints a line with put function
-*Return: 0 (success)
-*/
+ * main - programming is like
+ * Return: 0.
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
