d673984f469eed19d926e75400aeaec02ff31c43
Steye3 <teyesolomon427@gmail.com>
2023-02-19 09:53:02 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - printing alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+char le;
+for (le = 'z'; le >= 'a'; le--)
+putchar(le);
+putchar('\n');
+
+return (0);
+}
