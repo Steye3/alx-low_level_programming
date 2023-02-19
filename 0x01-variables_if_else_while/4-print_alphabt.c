b09cdf1c646ef3cda3a6fd75b485ec5fc79b024a
Steye3 <teyesolomon427@gmail.com>
2023-02-19 08:10:44 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la
+		for (la = 'a'; la <= 'z'; la++)
+		{
+			if (la != 'e' && la != 'q')
+				putchar(la);
+
+		}
+	putchar('\n');
+	return (0);
+}
