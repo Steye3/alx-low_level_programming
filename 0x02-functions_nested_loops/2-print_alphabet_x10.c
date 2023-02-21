6a176256f89135d8f93c5d5d06b975328aec6f82
Steye3 <teyesolomon427@gmail.com>
2023-02-21 03:12:16 -0800

    10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print all alphabet
+ *
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter * 10);
+_putchar('\n');
+}
