2a1ad3bd4d0566a75422805210d4fd0b0eb9b0ac
Steye3 <teyesolomon427@gmail.com>
2023-02-21 14:17:59 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main- _isalpha - checks for alphabets
+ * Return: Always 0
+ */
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+
+}
