c33034f36bd959e9f9807996b6ed3c23d71517cf
Steye3 <teyesolomon427@gmail.com>
2023-02-22 04:54:05 -0800

    ggd

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * main- print every minute of the day
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+int h1;
+int h2;
+int m1;
+int m2;
+int a = 9;
+
+h2 = 0;
+while (h2 <= 2)
+{
+	/*restrain to 23h, not 29*/
+	a = 3;
+
+}
+h1 = 0;
+while (h1 <= a)
+{
+	m2 = 0;
+	while (m2 <= 5)
+	{
+		m1 = 0;
+		while (m1 <= 9)
+		{
+			_putchar('0' + h2);
+			 _putchar('0' + h1);
+			  _putchar(':');
+			   _putchar('0' + m2);
+			    _putchar('0' + m1);
+			     _putchar('\n');
+			     m1++;
+		}
+		m2++
+
+	}
+	h1++
+}
+h2++
+}
+}
