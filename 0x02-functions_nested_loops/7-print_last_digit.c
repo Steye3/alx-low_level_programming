4ea13a47018af61e3504b601f6581b5d42c62a0b
Steye3 <teyesolomon427@gmail.com>
2023-02-22 02:50:51 -0800

    ryry

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,11 +4,14 @@
  *Return: Always 0
  */
 
-int print_last_digit(int)
+int print_last_digit(int r)
 {
-	int ld - n % 10;
-	if (ld < 0)
-		ld *= -1;
+int ld = r % 10;
+
+if (ld < 0)
+{
+ld *= -1;
 _putchar(ld + '0');
+}
 return (0);
 }
