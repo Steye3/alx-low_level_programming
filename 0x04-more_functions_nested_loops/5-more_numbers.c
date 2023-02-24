1436c4c46ebdf8ee42e03a1feffd00af9425099c
Steye3 <teyesolomon427@gmail.com>
2023-02-23 17:16:04 -0800

    god

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,6 +6,9 @@
 void more_numbers(void)
 {
 int i, j;
+
+for (i = 1; i <= 10; i++)
+{
 for (j = 1; j <= 10; j++)
 {
 if (j >= 10)
@@ -17,4 +20,5 @@ _putchar (j % 10 + '0');
 _putchar('\n');
 
 
+}
 }
