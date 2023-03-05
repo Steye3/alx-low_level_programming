feb2df07fd641f6e1fc3dda6f959820720ef954a
TEYE SOLOMON <teyesolomon427@gmail.com>
2023-03-05 05:20:26 -0800

    jsjs

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#inclue "main.h"
+
+/**
+* set_string - set value of pointer to string
+* @s: pointer to pointer to change
+* @to: pointer value
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
