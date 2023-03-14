9e9e8b2348bcc7f47bd3e04a3389b4b63b94a75b
TEYE SOLOMON <teyesolomon427@gmail.com>
2023-03-14 14:33:00 -0700

    copying string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _sttttrdup - copy a string to a newly allocated space in memory
+ * @str: string to copy
+ *
+ * Return: pointer to duplicated string or NULL if error
+ */
+
+char *_stdrup(char *str)
+{
+	unsigned int i, j;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	i++;
+
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
